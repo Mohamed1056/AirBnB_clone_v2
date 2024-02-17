6a23bf6ae8bcfbeb1c304b828f574b7c597184ef
Mohamed Gamal <mg8033667@gmail.com>
2024-02-17 12:57:16 -0800

    adding the 1st file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,42 @@
-#!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+#!/bin/bash
 
-apt-get update
-apt-get install -y nginx
+# Install Nginx if not already installed
+sudo apt-get update
+sudo apt-get -y install nginx
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Simple web page." > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+# Create necessary directories if they don't exist
+sudo mkdir -p /data/web_static/releases/test/
+sudo mkdir -p /data/web_static/shared/
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# Create a fake HTML file for testing
+echo "<html>
+<head>
+    <title>Test Page</title>
+</head>
+<body>
+    <h1>This is a test page.</h1>
+</body>
+</html>" | sudo tee /data/web_static/releases/test/index.html >/dev/null
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+# Create symbolic link and set ownership
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+sudo chown -R ubuntu:ubuntu /data/
+
+# Update Nginx configuration
+config="server {
+    listen 80;
+    server_name _;
 
     location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.html;
+        alias /data/web_static/current/;
     }
 
-    location /redirect_me {
-        return 301 https://github.com/Mohamed1056;
+    location / {
+        try_files $uri $uri/ =404;
     }
+}"
+echo "$config" | sudo tee /etc/nginx/sites-available/default >/dev/null
 
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
+# Restart Nginx
+sudo service nginx restart
 
-service nginx restart
