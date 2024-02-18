a6a414a40808da9f7ac618109ebf397c2efc2e66
Mohamed Gamal <mg8033667@gmail.com>
2024-02-18 13:29:06 -0800

    adding the 1-hbnb_route file

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,21 @@
+#!usr/bin/python3
+'''Script to use flask.'''
+
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def Hello_Hbnb():
+    return 'Hello HBNB!'
+
+
+@app.route("/hbnb", strict_slashes=False)
+def from_hbnb():
+    return 'HBNB'
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
