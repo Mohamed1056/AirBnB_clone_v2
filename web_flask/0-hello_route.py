5d5d5a77dc3a7e31905237bce6adcf92ee6b02e8
Mohamed Gamal <mg8033667@gmail.com>
2024-02-18 13:20:40 -0800

    fixing the 0-hello_route file

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,10 +6,12 @@ from flask import Flask
 # Create the Flask application instance
 app = Flask(__name__)
 
+
 # Route for the homepage
 @app.route('/', strict_slashes=False)
 def index():
     return 'Hello HBNB!'
+
 
 if __name__ == '__main__':
     # Run the Flask app
